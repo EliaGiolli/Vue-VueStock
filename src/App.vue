de684368cd5c611da0bc5f646adb1699ed1987c7
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2025-04-30 16:49:21 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <Navbar />
   <main class="bg-white ">
-    <div class="bg-gray-100 mt-10">
-      <h2 class="text-gray-900">Ciao</h2>
-    </div>
+    <InputForm @addProduct="addToInventory" />
+      <div class="flex flex-col justify-center items-center text-center gap-4 bg-white text-gray-900 w-full md:max-w-2xl min-h-[500px] text-center shadow-lg shadow-grey-900 rounded-lg p-8 my-20 mx-auto">
+        <h2 class="text-2xl md:text-3xl text-slate-900 font-bold p-2">Your inventory</h2>
+        <div v-for="(item, id) in inventory" :key="id" class="bg-white p-4 flex flex-col justify-around h-full">
+          <p class="text-lg p-2">{{ item.name }}</p>
+          <p class="text-lg p-2">Prezzo: €{{ item.price }}</p>
+          <p class="text-lg p-2">Quantità: {{ item.quantity }}</p>
+        </div>
+      </div>
   </main>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import Navbar from './Layouts/Navbar.vue';
+import InputForm from './Layouts/InputForm.vue';
+
+const inventory = ref([]);
+
+function addToInventory(productList) {
+  console.log('Product received:', productList);
+  inventory.value.push({
+    id:Date.now(),
+    ...productList});
+}
+
 </script>
---
+++ src/Layouts/InputForm.vue
@@ -0,0 +1,54 @@
+<template>
+<form action="GET" 
+    class="bg-white text-gray-900 w-full md:max-w-2xl h-fit flex flex-col justify-center items-center text-center gap-4 
+    shadow-lg shadow-grey-900 rounded-lg p-8 my-10 mx-auto"
+    @submit.prevent="handleSubmit"
+    >
+    <div class="flex gap-4 md:flex-col items-center text-center">
+        <label for="product-name" class="text-xl md:text-2xl text-gray-900">Nome prodotto</label>
+        <input type="text" name="Nome prodotto" id="Product-name" 
+            class="hover:bg-gray-100 border-2 border-gray-900 rounded-md focus:outline-emerald-500"
+            v-model.lazy.trim="productList.name"
+            >
+    </div>
+    <div class="flex gap-4 md:flex-col items-center text-center">
+        <label for="product-quantity" class="text-xl md:text-2xl text-gray-900">Quantità</label>
+        <input type="number" name="" id="product-quantity" 
+            class="hover:bg-gray-100 border-2 border-gray-900 rounded-md focus:outline-emerald-500"
+            v-model.lazy.number.trim="productList.quantity"
+            >
+    </div>
+    <div class="flex gap-4 md:flex-col items-center text-center">
+        <label for="product-price" class="text-xl md:text-2xl text-gray-900">Prezzo</label>
+        <input type="number" name="" id="product-price" 
+            class="hover:bg-gray-100 border-2 border-gray-900 rounded-md focus:outline-emerald-500"
+            v-model.lazy.number.trim="productList.price"
+            >
+    </div>
+    <button 
+        class="bg-emerald-500 hover:bg-emerald-600 text-white text-lg rounded-lg shadow-lg shadow-grey-900 p-4 my-5"
+        type="submit">
+        Aggiungi prodotto
+    </button>
+
+</form>
+
+
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const emit = defineEmits(['addProduct'])
+
+
+const productList = reactive({
+    name: '',
+    price: 0,
+    quantity:0,
+});
+
+const handleSubmit = (e)=>{
+    emit('addProduct', productList);
+}
+</script>
