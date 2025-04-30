e5d2bb9cd67609b4f5a1ae1cb1469578279a5db6
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2025-04-30 19:05:55 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <main class="bg-white ">
     <InputForm @addProduct="addToInventory" />
       <div class="flex flex-col justify-center items-center text-center gap-4 bg-white text-gray-900 w-full md:max-w-2xl min-h-[500px] text-center shadow-lg shadow-grey-900 rounded-lg p-8 my-20 mx-auto">
-        <h2 class="text-2xl md:text-3xl text-slate-900 font-bold p-2">Your inventory</h2>
+        <h2 class="text-2xl md:text-3xl text-slate-900 font-bold p-2">Il tuo inventario</h2>
         <div v-for="(item, id) in inventory" :key="id" class="bg-white p-4 flex flex-col justify-around h-full">
           <p class="text-lg p-2">{{ item.name }}</p>
           <p class="text-lg p-2">Prezzo: €{{ item.price }}</p>
