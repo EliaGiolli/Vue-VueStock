c655c41b56c1f99f45b5fd9f5d1d58694c614c3b
Elia Giolli <you@example.com>
2025-04-30 14:05:10 +0200

    project initialized

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Navbar />
+  <main class="bg-white ">
+    <div class="bg-gray-100 mt-10">
+      <h2 class="text-gray-900">Ciao</h2>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Navbar from './Layouts/Navbar.vue';
+</script>
---
+++ src/Layouts/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+<nav class="w-full h-20 p-3 flex justify-around items-center text-center bg-green-200">
+    <p class="text-2xl uppercase">LOGO</p>
+    <ul class="flex items-center">
+      <li><a href="/">About</a></li>
+      <li><a href="/">Tours</a></li>
+      <li><a href="/">Contacts</a></li>
+    </ul>
+    <button class="bg-emerald-500 hover:bg-emerald-600 p-2 rounded-lg">Click</button>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
