5984c40de01e74c13b09b635a1aee4fbd85dd917
Elia Giolli <you@example.com>
2025-07-06 12:32:05 +0200

    change button and navbar background color

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,13 @@
     <Navbar />
     
     <main class="container mx-auto py-8">
+      <section class="flex flex-col items-center justify-center border-l-4 border-l-emerald-500 p-4 my-10 mx-auto">
+        <h1 class="text-emerald-800 text-left text-2xl font-bold">VueStock - Gestione del magazzino virtuale</h1>
+        <p class="text-left">VueStock è un'applicazione web che permette di gestire un magazzino virtuale.
+          <br>
+          Inserisci i prodotti nel magazzino e visualizza l'inventario.
+        </p>
+      </section>
       <InputForm @addProduct="handleAddProduct" />
       <InventoryItems ref="inventoryRef" />
     </main>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="w-full h-20 p-3 flex justify-around items-center text-center bg-green-200 sticky top-0 border-b-2 border-b-emerald-600">
+<nav class="w-full h-20 p-3 flex justify-around items-center text-center bg-emerald-500 sticky top-0 border-b-2 border-b-emerald-600">
     <p class="text-2xl uppercase font-bold">LOGO</p>
     <!-- DESKTOP MENU-->
     <ul class="hidden md:flex items-center gap-3">
@@ -8,6 +8,7 @@
       <li><a href="/">Contatti</a></li>
     </ul>
     <Button 
+      variant="navbar"
       size="md"
       @click="isOpen = !isOpen"
       >
